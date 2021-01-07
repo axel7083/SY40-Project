026e2c47c80698829bb352c9baa3c2ab706661de
axel7083 <42176370+axel7083@users.noreply.github.com>
2021-01-07 14:51:06 +0100

    Fix indentation

--- src/utils.c
+++ src/utils.c
@@ -62,40 +62,40 @@ void log_(char TAG[], char *fmt, ...)
     else
         id =  pthread_self();
         
-	printf("\n[%s][%d] ", TAG, id);
-	va_list ap;
-	char *p, *sval;
-	int ival;
-	double dval;
-	va_start(ap, fmt);
-	for (p = fmt; *p; p++)
-	{
-		if (*p != '%')
-		{
-			putchar(*p);
-			continue;
-		}
-		switch (*++p)
-		{
-			case 'd':
-				ival = va_arg(ap, int);
-				printf("%d", ival);
-				break;
-			case 'f':
-				dval = va_arg(ap, double);
-				printf("%f", dval);
-				break;
-			case 's':
-				for (sval = va_arg(ap, char *); *sval; sval++)
-					putchar(*sval);
-				break;
-			default:
-				putchar(*p);
-				break;
-		}
-	}
-	va_end(ap); /*clean up when done */
-	fflush(stdout);
+    printf("\n[%s][%d] ", TAG, id);
+    va_list ap;
+    char *p, *sval;
+    int ival;
+    double dval;
+    va_start(ap, fmt);
+    for (p = fmt; *p; p++)
+    {
+        if (*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+       switch (*++p)
+        {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+            break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+            break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+            break;
+        }
+    }
+    va_end(ap); /*clean up when done */
+    fflush(stdout);
 }
 
 int isNumber(char *tmp)
